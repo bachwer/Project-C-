afbc860f18c0e8d1e34983ae924952f9faadd3ed
bachwer <110678064+bachwer@users.noreply.github.com>
2025-01-02 20:30:27 +0700

    Add files via upload

--- Project/main.c
+++ Project/main.c
@@ -2,6 +2,7 @@
 #include "include/function.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 int main(void) {
     int menu, menu1;
     //for Student
@@ -10,15 +11,16 @@ int main(void) {
     int nT;
     //for Class
     int nC;
-
-    login();
+    adminlogin();
+    system("clear");
     menu0:
+    system("clear");
     menuStart(&menu);
     switch (menu) {
         //Student
         case 1:
             system("clear");
-                menu11:
+                menustudent:
                 menuStudent(&menu);
                 switch (menu) {
                     case 1:
@@ -31,7 +33,6 @@ int main(void) {
                     goto menu1;
 
                     case 2:
-                        system("clear");
                         dataLoadS(&student, &nS);
                     showStudent(&student, &nS);
                     goto menu1;
@@ -45,7 +46,7 @@ int main(void) {
                     case 4:
                         system("clear");
                         dataLoadS(&student, &nS);
-                    editStudent(&student, &nS);
+                    editStudent(&student, &nS, &class, &nC);
                     dataSaveS(&student, &nS);
                     goto menu1;
 
@@ -76,13 +77,14 @@ int main(void) {
                         scanf("%d", &menu1);
                     }while(menu1 != 1 && menu1 != 2);
                     if(menu1 == 1) {
-                        goto menu11;
+                        goto menustudent;
                     }else {
                         return 1;
                     }
                 }
         //Teacher
         case 2:
+            menusteacher:
             system("clear");
                 menuTeacher(&menu);
                 switch (menu) {
@@ -94,43 +96,56 @@ int main(void) {
                         addTeacher(&teachers, &nT, &class, &nC);
                         dataSaveT(&teachers, &nT);
                         dataSaveC(&class, &nC);
+                    goto menu2;
 
-                        break;
 
                     case 2:
                         dataLoadT(&teachers, &nT);
                     showTeacher(&teachers, &nT);
-                        break;
+                    goto menu2;
 
                     case 3:
                         dataLoadT(&teachers, &nT);
                     searchTeacher(&teachers, &nT);
-                        break;
+                    goto menu2;
+
 
                     case 4:
+                        dataLoadC(&class, &nC);
                         dataLoadT(&teachers, &nT);
-                        editTeacher(&teachers, &nT);
+                        editTeacher(&teachers, &nT,&class, &nC);
                          dataSaveT(&teachers, &nT);
-                        break;
+                    goto menu2;
 
                     case 5:
                         dataLoadT(&teachers, &nT);
                         deleteTeacher(&teachers, &nT);
                     dataSaveT(&teachers, &nT);
-                        break;
+                    goto menu2;
 
                     case 6:
-
-                        break;
+                        system("clear");
+                    goto menu0;
 
                     default:
                         printf("Exited !!!");
                     return 1;
+                    menu2:
+                    do {
+                        printf("1.Go Back \n");
+                        printf("2.Exit \n");
+                        scanf("%d", &menu1);
+                    }while(menu1 != 1 && menu1 != 2);
+                    if(menu1 == 1) {
+                        goto menusteacher;
+                    }else {
+                        return 1;
+                    }
 
         }
-        break;
         //Class
         case 3:
+            menuclass:
             system("clear");
                 menuClass(&menu);
                 switch (menu) {
@@ -139,44 +154,60 @@ int main(void) {
                         dataLoadC(&class, &nC);
                         addClass(&class, &nC);
                         dataSaveC(&class, &nC);
-                        break;
+                    goto menu3;
 
                     case 2:
                         system("clear");
                         dataLoadC(&class, &nC);
                     showClass(&class, &nC);
-                        break;
+                    goto menu3;
 
                     case 3:
                         dataLoadC(&class, &nC);
                         editClass(&class, &nC);
                     dataSaveC(&class, &nC);
-                        break;
+                    goto menu3;
 
                     case 4:
                         dataLoadC(&class, &nC);
                         deleteClass(&class, &nC);
                     dataSaveC(&class, &nC);
-                        break;
+                    goto menu3;
 
                     case 5:
                         dataLoadC(&class, &nC);
                         dataLoadS(&student, &nS);
                         detailslass(&class, &nC, &student, &nS);
-                        break;
+                    goto menu3;
+
+                    case 6:
+                        dataLoadC(&class, &nC);
+                        dataLoadS(&student, &nS);
+                        deleteStudenForclass(&class, &nC, &student, &nS);
+                        dataSaveC(&class, &nC);
+                        dataSaveS(&student, &nS);
+                    goto menu3;
+
+                    case 7:
+                        system("clear");
+                    goto menu0;
                     default:
                         printf("Exited !!!");
                     return 1;
+                    menu3:
+                    do {
+                        printf("1.Go Back \n");
+                        printf("2.Exit \n");
+                        scanf("%d", &menu1);
+                    }while(menu1 != 1 && menu1 != 2);
+                    if(menu1 == 1) {
+                        goto menuclass;
+                    }else {
+                        return 1;
+                    }
                 }
-
-
-            break;
-
-
-
         default:
             printf("Exited !!!");
-            return 1;
+            return 0;
     }
-    return 0;
 }
